08f35293739a424a135d65f74339f8067eb1dd87
igorwang <igorwangyi@gmail.com>
2024-05-10 09:25:15 +0800

    login: Authentik

---
+++ auth.ts
@@ -0,0 +1,81 @@
+import NextAuth from "next-auth";
+
+import Credentials from "next-auth/providers/credentials";
+import Authentik from "next-auth/providers/authentik";
+
+import { use } from "react";
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "user@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        const loginData = {
+          email: email,
+          password: password,
+        };
+
+        const apiUrl = process.env.API_URL;
+        const response = await fetch(`${apiUrl}/v1/user/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(loginData),
+        }).catch((error) => {
+          console.error("Network error:", error);
+          throw new Error(
+            "Unable to connect to the server. Please try again later."
+          );
+        });
+        const user = await response.json();
+        if (response.status == 200) {
+          return {
+            email: email,
+            access_token: user?.access_token,
+          };
+        }
+        return null;
+      },
+    }),
+    Authentik({
+      clientId: process.env.AUTH_AUTHENTIK_CLIENT_ID,
+      clientSecret: process.env.AUTH_AUTHENTIK_CLIENT_SECRET,
+      issuer: process.env.AUTH_AUTHENTIK_ISSUER,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: async ({ token, user, session }) => {
+      console.log("jwt callback");
+      console.log(user)
+      if (user) {
+        token.email = user.email;
+        token.access_token = user.access_token;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      console.log("session callback");
+      session.access_token = token.access_token;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+});
---
+++ types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import "next-auth";
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  // Extending the built-in User model
+  interface User {
+    access_token?: string;
+  }
+
+  interface Session {
+    access_token?: string;
+  }
+  
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    access_token?: string;
+  }
+}
