1fe1e669a0b727cc0ce123831c32e4a46e709c29
igorwang <igorwangyi@gmail.com>
2024-06-14 22:50:48 +0800

    router path

---
+++ app/(chat)/chat/[id]/page.tsx
@@ -0,0 +1,23 @@
+import ChatHub from "@/components/AgentHub";
+import { Conversation } from "@/components/Conversation";
+import StoreProvider from "@/app/StoreProvider";
+
+export default function ChatPage() {
+  // const session = await auth()
+  // console.log(session)
+  const bot = {
+    id: 1,
+    name: "hello world",
+    description: "hello world",
+    avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/3.png",
+  };
+
+  return (
+    <div className="flex flex-row h-full w-full">
+      <StoreProvider>
+        <ChatHub></ChatHub>
+        <Conversation></Conversation>
+      </StoreProvider>
+    </div>
+  );
+}
