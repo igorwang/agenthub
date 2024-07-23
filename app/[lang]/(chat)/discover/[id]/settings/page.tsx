ca1e7880203ddad9cdc5e79446e2047f784dbb60
igorwang <igorwangyi@gmail.com>
2024-07-23 11:49:43 +0800

    refactor db config

--- app/[lang]/(chat)/discover/[id]/settings/page.tsx
+++ app/[lang]/(chat)/discover/[id]/settings/page.tsx
@@ -1,6 +1,6 @@
 export default function LibrarySettingPage() {
   return (
-    <div className="mx-auto flex h-full w-full max-w-3xl flex-col items-center justify-center px-4 py-6 sm:px-6 md:px-8 lg:px-12">
+    <div className="mx-auto flex h-full w-full items-center px-10 py-10">
       {/* <LibraryFile id={id} showSaveButton={true} /> */}
       <div>LibrarySettingPage</div>
     </div>
