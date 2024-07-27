308ac302ecc1112ae501add21d4314100c79eef0
igorwang <igorwangyi@gmail.com>
2024-07-27 21:15:12 +0800

    change library page

---
+++ app/[lang]/(chat)/library/[id]/workflow/page.tsx
@@ -0,0 +1,69 @@
+import { auth } from "@/auth";
+import WorkflowForm from "@/components/WorkflowForm";
+import {
+  GetNodeTypeListDocument,
+  GetNodeTypeListQuery,
+  GetNodeTypeListQueryVariables,
+  KnowledgeBaseDetailDocument,
+  KnowledgeBaseDetailQuery,
+  KnowledgeBaseDetailQueryVariables,
+} from "@/graphql/generated/types";
+import { bindWorkflowToLibrary } from "@/lib/actions/workflowActions";
+import { fetchData } from "@/lib/apolloRequest";
+import { v4 } from "uuid";
+
+async function fetchLibraryData(id: string) {
+  const session = await auth();
+  if (!session?.user) return null;
+
+  const variables: KnowledgeBaseDetailQueryVariables = {
+    id: id,
+  };
+
+  return await fetchData<KnowledgeBaseDetailQuery, KnowledgeBaseDetailQueryVariables>(
+    KnowledgeBaseDetailDocument,
+    variables,
+  );
+}
+
+async function getNodeTypeListData() {
+  const session = await auth();
+  if (!session?.user) return null;
+
+  const variables: GetNodeTypeListQueryVariables = {};
+
+  return await fetchData<GetNodeTypeListQuery, GetNodeTypeListQueryVariables>(
+    GetNodeTypeListDocument,
+    variables,
+  );
+}
+
+export default async function LibraryWorkflowPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const library = await fetchLibraryData(params.id);
+  const nodeTypeList = await getNodeTypeListData();
+  const workflow = library?.knowledge_base_by_pk?.workflow;
+  const initialData = {
+    id: workflow?.id || v4(), //workflow id
+    name: workflow?.name || "New Workflow",
+    description: workflow?.description || "New Workflow",
+    nodes: workflow?.flow_nodes,
+    edges: workflow?.flow_edges,
+  };
+  console.log(initialData);
+
+  const bindWorkflowToLibraryWithId = bindWorkflowToLibrary.bind(null, params.id);
+
+  return (
+    <div className="mx-auto h-full w-full min-w-[600px] px-20">
+      <WorkflowForm
+        initialData={initialData}
+        action={bindWorkflowToLibraryWithId}
+        nodeTypeList={nodeTypeList?.node_type || []}
+      />
+    </div>
+  );
+}
---
+++ components/ui/coming-soon-page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { Button, Card, Input } from "@nextui-org/react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+
+const ComingSoonPage: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted email:", email);
+    setEmail("");
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center p-5">
+      <Card className="w-full max-w-md p-5">
+        <h1 className="mb-4 text-center text-4xl font-bold">Coming Soon</h1>
+        <h2 className="mb-4 text-center text-2xl">
+          We're working hard to bring you something amazing.
+        </h2>
+        <p className="mb-8 text-center">
+          Be the first to know when we launch. Subscribe to our newsletter for updates.
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            fullWidth
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleEmailChange}
+            aria-label="Email input"
+          />
+          <Button type="submit" className="w-full">
+            Notify Me
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default ComingSoonPage;
